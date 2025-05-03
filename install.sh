91406313714d3153151ccd248b0ff30d4a098eee
bingandabong <maverickfia29@outlook.com>
2025-05-03 10:02:36 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,9 +14,9 @@ cp -r BlobeVM/root/config/* Save
 
 json_file="BlobeVM/options.json"
 if jq ".enablekvm" "$json_file" | grep -q true; then
-    docker run -d --name=BlobeVM -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=BlobeVM -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped blobevm
+    docker run -d --name=BlobeVM -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=BlobeVM -p 3000:3000 --shm-size="500gb" -v $(pwd)/Save:/config --restart unless-stopped blobevm
 else
-    docker run -d --name=BlobeVM -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=BlobeVM -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped blobevm
+    docker run -d --name=BlobeVM -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=BlobeVM -p 3000:3000 --shm-size="500gb" -v $(pwd)/Save:/config --restart unless-stopped blobevm
 fi
 clear
 echo "BLOBEVM WAS INSTALLED SUCCESSFULLY! Check Port Tab"
